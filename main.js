25e4643e0379db3b3cf0825255a4b42502d4d207
Wayne Boka <boka.wayne@gmail.com>
2015-03-12 19:15:21 -0400

    Adds success handler

--- main.js
+++ main.js
@@ -25,6 +25,9 @@ $(document).ready(function() {
 
 function runTests() {
 	$.ajax({
-		url: 'qUnitTests.html'
+		url: 'qUnitTests.html',
+		success: function(qUnitTests) {
+			$("#qUnitDiv").html(qUnitTests);
+		}
 	});
 }
