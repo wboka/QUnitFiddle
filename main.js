0d90b8d3f68b4acc608670ea3542380e2c2e7d1c
wboka <boka.wayne@gmail.com>
2015-07-17 21:01:29 -0400

    - Adds run example  button - Adds reset button - Adds facebook buttons

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+var exampleTest = 'QUnit.test("Hello Test", function(assert) {\n    assert.ok(true, "Passed!");\n});';
+
 $(document).ready(function() {
+    $("#reset").on("click", resetTests);
     $("#run").on("click", runTests);
+    $("#example").on("click", runExample);
 
     $("textarea").keydown(function(e) {
         if(e.keyCode === 9) { // tab was pressed
@@ -23,8 +27,12 @@ $(document).ready(function() {
     });
 
     $(document).on("keydown", function(e) {
-        if ((e.keyCode == 10 || e.keyCode == 13) && e.ctrlKey) {
-            $("#run").click();
+        if ((e.keyCode == 10 || e.keyCode == 13) && e.ctrlKey && e.shiftKey) {
+            runExample();
+        } else if ((e.keyCode == 10 || e.keyCode == 13) && e.ctrlKey) {
+            runTests();
+        } else if (e.keyCode == 82 && e.shiftKey) {
+            resetTests();
         }
     });
 
@@ -34,8 +42,19 @@ $(document).ready(function() {
     });
 });
 
+function resetTests() {
+    $("#tests").val("");
+    document.location.reload();
+}
+
 function runTests() {
     if ($("#tests").val().length > 0) {
         $("#testsContainer").append("<script type='text/javascript'>" + $("#tests").val() + "</script>");
     }
+}
+
+function runExample() {
+    $("#tests").val(exampleTest);
+
+    runTests();
 }
